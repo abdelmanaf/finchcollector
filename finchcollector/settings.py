c4879b5930595b80b675a1c214d7c257034c93a9
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-24 15:57:12 -0500

    add dotenv file

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -1,3 +1,6 @@
+import environ
+env = environ.Env()
+environ.Env.read_env()
 """
 Django settings for finchcollector project.
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^rg^vh$f&2$o-3lqig_5y-q!s)(65m(5#u^!&&v_jiz#cw7-c5'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
