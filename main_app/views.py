938faf48f3bf0434e341b11f16658c0a7d247f3d
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-24 18:29:50 -0500

    add super user

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 
 from django.http import request
+from .models import Finch
 
-
-
-def finches_index(request):
-  return render(request, 'finches/index.html', {'finches': finches})
 
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def finches_index(request):
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', {'finches': finches})
