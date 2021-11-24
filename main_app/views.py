8b8cf57cea54c4d30545566ad79d0060938a402e
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-24 16:54:05 -0500

    add finch UI

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, request
 
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
+  Finch('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Finch('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Finch('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+def finches_index(request):
+  return render(request, 'cats/index.html', {'finches': finches})
 
 def home(request):
   return HttpResponse('<h1> Hello </h1>')
