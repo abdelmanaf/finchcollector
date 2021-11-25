a1cc4b4f1a24cdcb305c9ca8945dba97f4fd13dd
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-24 19:35:55 -0500

    add finches detail page

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
 class Finch(models.Model):
-  name = models.CharField(max_length=100),
-  breed = models.CharField(max_length=100),
-  description = models.TextField(max_length=250),
+  name = models.CharField(max_length=100, null=True)
+  breed = models.CharField(max_length=100, null=True)
+  description = models.TextField(max_length=250, null=True)
   age = models.IntegerField()
 
   def __str__(self):
     return self.name
-
-
