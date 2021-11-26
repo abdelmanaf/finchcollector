3e5323057d5b8f14c8942a824857d53a824d72bd
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-26 18:24:58 -0500

    add many to many relationship

--- main_app/models.py
+++ main_app/models.py
@@ -9,11 +9,23 @@ MEALS = (
   ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Finch(models.Model):
   name = models.CharField(max_length=100, null=True)
   breed = models.CharField(max_length=100, null=True)
   description = models.TextField(max_length=250, null=True)
   age = models.IntegerField()
+
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return self.name
@@ -42,14 +54,6 @@ class Feeding(models.Model):
   class Meta:
     ordering = ['-date']
 
-class Toy(models.Model):
-  name = models.CharField(max_length=50)
-  color = models.CharField(max_length=20)
 
-  def __str__(self):
-    return self.name
-
-  def get_absolute_url(self):
-    return reverse('toys_detail', kwargs={'pk': self.id})
   
   
