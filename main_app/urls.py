4b14963ac8348d97662228903822a13d4523517d
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-27 15:21:19 -0500

    add signup functionalite

--- main_app/urls.py
+++ main_app/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
   
+  path('accounts/signup/', views.signup, name='signup'),
 ]
