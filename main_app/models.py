47b0947fc1fb9dd0ea43f758dcbf617742e59899
abdelmanaf <ourotoumanaf6@gmail.com>
2021-11-27 13:37:03 -0500

    add user

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.enums import Choices
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 MEALS = (
   ('B', 'Breakfast'),
@@ -26,6 +27,7 @@ class Finch(models.Model):
   age = models.IntegerField()
 
   toys = models.ManyToManyField(Toy)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
